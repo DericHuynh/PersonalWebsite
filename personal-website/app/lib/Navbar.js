9050791cb6067047ed8a306b101b133b5a939490
DericHuynh <dhuynh17@nait.ca>
2024-05-08 01:29:44 -0600

    Centered name in navbar.

--- personal-website/app/lib/Navbar.js
+++ personal-website/app/lib/Navbar.js
@@ -14,8 +14,8 @@ export default function Navbar() {
                 leaveFrom="translate-x-0"
                 leaveTo="-translate-x-full"
             >
-                <DisclosurePanel className="px-2 flex flex-col gap-3 text-white border-r-2 border-gray-800">
-                    <p className="w-max mt-4 underline">Deric Huynh</p>
+                <DisclosurePanel className="px-2 flex flex-col gap-3 text-white border-r-2  border-gray-800">
+                    <p className="w-full mt-4 underline text-center">Deric Huynh</p>
                     <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/about"><ListBulletIcon className="size-5"/>About</Link>
                     <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/resume"><ClipboardDocumentListIcon className="size-5"/>Resume</Link>
                     <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/projects"><CodeBracketSquareIcon className="size-5" />Projects</Link>
