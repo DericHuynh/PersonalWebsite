a0360dd3a0bd2425fc9c1220dc886461c515ad47
DericHuynh <dhuynh17@nait.ca>
2024-05-08 01:36:35 -0600

    Updated navbar to navigate to home instead of about me.

--- personal-website/app/lib/Navbar.js
+++ personal-website/app/lib/Navbar.js
@@ -1,6 +1,6 @@
 import { Button, Disclosure, DisclosureButton, DisclosurePanel, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from '@heroicons/react/24/solid'
-import { CodeBracketSquareIcon, Cog8ToothIcon, DocumentIcon, ListBulletIcon, ClipboardDocumentListIcon } from '@heroicons/react/24/outline'
+import { CodeBracketSquareIcon, Cog8ToothIcon, DocumentIcon, HomeIcon, ClipboardDocumentListIcon } from '@heroicons/react/24/outline'
 import Link from "next/link";
 
 export default function Navbar() {
@@ -16,7 +16,7 @@ export default function Navbar() {
             >
                 <DisclosurePanel className="px-2 flex flex-col gap-3 text-white border-r-2  border-gray-800">
                     <p className="w-full mt-4 underline text-center">Deric Huynh</p>
-                    <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/about"><ListBulletIcon className="size-5"/>About</Link>
+                    <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/"><HomeIcon className="size-5"/>Home</Link>
                     <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/resume"><ClipboardDocumentListIcon className="size-5"/>Resume</Link>
                     <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/projects"><CodeBracketSquareIcon className="size-5" />Projects</Link>
                     <Link className="px-1 bg-slate-800 shadow rounded hover:bg-gray-700 flex flex-row items-center gap-1.5 pr-1.5" href="/activities"><DocumentIcon className="size-5" />Activites</Link>
@@ -24,7 +24,7 @@ export default function Navbar() {
                 </DisclosurePanel>
             </Transition>
             <DisclosureButton className="group my-auto">
-                <ChevronDownIcon className="transition duration-400 relative size-5 text-white group-data-[open]:rotate-90 -rotate-90" />
+                <ChevronDownIcon className="transition duration-500 relative size-5 text-white group-data-[open]:rotate-90 -rotate-90" />
             </DisclosureButton>
         </Disclosure>
     );
