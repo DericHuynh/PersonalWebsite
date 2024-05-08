faef3335bd931333fcf15c709b10d7f53df6af0c
DericHuynh <dhuynh17@nait.ca>
2024-05-08 01:39:41 -0600

    Strongly typed apostrophe because ESLint is screaming about it.

--- personal-website/app/page.js
+++ personal-website/app/page.js
@@ -10,10 +10,10 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-2">
           <p className="indent-5">
-            I'm a Computer Engineering Technology student at the Northern Alberta Institute of Technology, currently maintaining an impressive 3.8 GPA. Based in Edmonton, my academic and professional journey is characterized by deep technical expertise and innovative problem-solving.
+            I&apos;m a Computer Engineering Technology student at the Northern Alberta Institute of Technology, currently maintaining an impressive 3.8 GPA. Based in Edmonton, my academic and professional journey is characterized by deep technical expertise and innovative problem-solving.
           </p>
           <p className="indent-5">
-            In the realm of technology, I've engaged in diverse projects, encompassing web and software development, embedded systems, electronic and PCB design, and database management.
+            In the realm of technology, I&apos;ve engaged in diverse projects, encompassing web and software development, embedded systems, electronic and PCB design, and database management.
           </p>
           <p className="indent-5">
             A significant focus of my personal projects has been the development of smart home systems. I successfully engineered a user-friendly website for smart home device management, utilizing Svelte and Tailwind CSS for the frontend, which achieved remarkably fast page load times. Additionally, I co-engineered an integrated smart home system, programming devices for varied functionalities, which communicated via the MQTT protocol with exceptional responsiveness. These projects underscore my ability to integrate front-end design with sophisticated back-end solutions, optimizing both user experience and system efficiency.
@@ -25,7 +25,7 @@ export default function Home() {
             Beyond technical pursuits, I actively participate in the Edmonton Advanced Toastmasters club, enhancing my public speaking skills and contributing to workshops and speeches that have been consistently lauded by senior members. My entrepreneurial spirit was also cultivated through two years of involvement in Junior Achievement, where I developed and marketed products as part of a small business team.
           </p>
           <p className="indent-5">
-            I am keen to leverage my technical skills, analytical capabilities, and proactive approach in a professional setting, eager to contribute to innovative engineering solutions. Let's connect to discuss how we can collaborate on future technology projects.
+            I am keen to leverage my technical skills, analytical capabilities, and proactive approach in a professional setting, eager to contribute to innovative engineering solutions. Let&apos;s connect to discuss how we can collaborate on future technology projects.
           </p>
         </div>
       </div>
