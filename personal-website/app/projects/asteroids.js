ff50dd6a2520b047a10d82ad89acc768c8279ccc
DericHuynh <dhuynh17@nait.ca>
2024-05-09 22:10:00 -0600

    Added white background to asteroids iframe for initial blank area on load.

--- personal-website/app/projects/asteroids.js
+++ personal-website/app/projects/asteroids.js
@@ -7,7 +7,7 @@ export default function Asteroids() {
       <p className="w-full bg-white/10  px-2">
         An Asteroids recreation, made in Java using the Eclipse IDE. JFrame and Polygon classes used for rendering, my grade 12 computer science final project. 
       </p>
-      <iframe className="h-full w-full rounded-b-md" src="https://www.youtube.com/embed/IwzN57SE_xU?si=m3N3JixWut_JfSgC&vq=hd1080p60&origin=https://huynhderic.net" title="YouTube video player" allow="fullscreen; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe className="h-full w-full rounded-b-md bg-white/10" src="https://www.youtube.com/embed/IwzN57SE_xU?si=m3N3JixWut_JfSgC&vq=hd1080p60&origin=https://huynhderic.net" title="YouTube video player" allow="fullscreen; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   );
 }
